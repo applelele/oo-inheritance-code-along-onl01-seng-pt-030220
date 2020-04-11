2015b6abc02f3a7f9a8a41a56e7a7ab0690d9acd
YUKIKO IKEDA <applelele@gmail.com>
2020-04-11 16:09:25 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,11 @@
 class Vehicle
+  #parent class - subclasses will have access to these methods
+
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
 
 end
